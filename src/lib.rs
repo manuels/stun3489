a3b7c5b945acb8d7fd6f5a167972d73c972175da
Manuel Schölling <manuel.schoelling@gmx.de>
2017-09-10 16:13:26 +0200

    Remove UdpBlocked and use futures-await

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,39 @@
+#![feature(proc_macro, conservative_impl_trait, generators)]
+
 extern crate byteorder;
-extern crate futures;
+extern crate futures_await as futures;
 extern crate tokio_core;
 extern crate ring;
 extern crate rand;
-extern crate mio;
 #[macro_use] extern crate log;
 
 mod codec;
+mod request;
 
 use std::io;
 use std::io::Error;
-use std::io::Result;
 use std::io::ErrorKind;
 use std::net::SocketAddr;
 use std::time::Duration;
-
-use rand::Rng;
 
 use tokio_core::net::UdpCodec;
 use tokio_core::net::UdpSocket;
 use tokio_core::reactor::Handle;
 use tokio_core::reactor::Timeout;
 
+use futures::prelude::*;
 use futures::stream::Stream;
 use futures::sink::Sink;
-use futures::IntoFuture;
 
 use futures::Future;
-use futures::Flatten;
-use futures::Poll;
-use futures::Async;
-use futures::future::FutureResult;
-use futures::future::ok;
-use futures::future::err;
 
 use codec::Response;
 use codec::BindRequest;
 use codec::ChangeRequest;
+
+use request::Request;
+
+pub const NETWORK_UNREACHABLE:i32 = 101;
 
 #[derive(Debug, PartialEq)]
 pub enum Connectivity {
@@ -46,7 +43,6 @@ pub enum Connectivity {
     RestrictedPortNat(SocketAddr),
     RestrictedConeNat(SocketAddr),
     SymmetricFirewall(SocketAddr),
-    UdpBlocked,
 }
 
 impl Into<Option<SocketAddr>> for Connectivity {
@@ -58,138 +54,7 @@ impl Into<Option<SocketAddr>> for Connectivity {
             Connectivity::RestrictedPortNat(addr) => Some(addr),
             Connectivity::RestrictedConeNat(addr) => Some(addr),
             Connectivity::SymmetricFirewall(addr) => Some(addr),
-            Connectivity::UdpBlocked => None,
         }
-    }
-}
-
-struct Request<S,T> {
-    id:      u64,
-    sink:    Option<S>,
-    stream:  Option<T>,
-    codec:   codec::StunCodec,
-    addr:    SocketAddr,
-    sent:    bool,
-    request: codec::Request,
-    timeout: Flatten<FutureResult<Timeout, Error>>,
-}
-
-impl<S,T> Request<S,T>
-    where S: Sink<SinkItem=(Vec<u8>, SocketAddr), SinkError=io::Error>,
-          T: Stream<Item=(Vec<u8>, SocketAddr), Error=io::Error>
-{
-    fn new(sink:    S,
-           stream:  T,
-           dst:     SocketAddr,
-           request: codec::Request,
-           timeout: Result<Timeout>)
-       -> Self
-    {
-        let mut rng = rand::thread_rng();
-
-        Request {
-            sink:    Some(sink),
-            stream:  Some(stream),
-            addr:    dst,
-            request: request,
-            timeout: timeout.into_future().flatten(),
-            sent:    false,
-            id:      rng.gen::<u64>(),
-            codec:   codec::StunCodec::new(),
-        }
-    }
-
-    pub fn mut_sink(&mut self) -> &mut S {
-        self.sink.as_mut().expect("Attempted Request::mut_sink after completion")
-    }
-
-    pub fn mut_stream(&mut self) -> &mut T {
-        self.stream.as_mut().expect("Attempted Request::mut_stream after completion")
-    }
-}
-
-impl<S,T> Future for Request<S,T>
-    where S: Sink<SinkItem=(Vec<u8>, SocketAddr), SinkError=io::Error>,
-          T: Stream<Item=(Vec<u8>, SocketAddr), Error=io::Error>
-{
-    type Item = (S, T, Option<Response>);
-    type Error = (S, T, io::Error);
-
-    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-        if !self.sent {
-            let msg = (self.id, self.addr, self.request.clone());
-
-            let mut buf = Vec::with_capacity(2048);
-            let dst = self.codec.encode(msg, &mut buf);
-            assert_eq!(dst, self.addr);
-
-            let len = buf.len();
-
-            match self.mut_sink().send((buf, dst)).poll() {
-                Ok(Async::Ready(_)) => {
-                    debug!("sent {} bytes to {:?}", len, dst);
-                    self.sent = true;
-                },
-                Ok(Async::NotReady) => (),
-                Err(e) => return Err((self.sink.take().unwrap(),
-                                      self.stream.take().unwrap(),
-                                      e)),
-            }
-        }
-
-        loop {
-            match self.mut_stream().poll() {
-                Ok(Async::NotReady) => break,
-                Err(e) => return Err((self.sink.take().unwrap(),
-                                      self.stream.take().unwrap(),
-                                      e)),
-                Ok(Async::Ready(None)) => {
-                    return Ok(Async::Ready((self.sink.take().unwrap(),
-                                            self.stream.take().unwrap(),
-                                            None)));
-                },
-                Ok(Async::Ready(Some((buf, src)))) => {
-                    if let Ok((id, response)) = self.codec.decode(&src, &buf[..]) {
-                        if self.id == id {
-                            return Ok(Async::Ready((self.sink.take().unwrap(),
-                                                    self.stream.take().unwrap(),
-                                                    Some(response))));
-                        }
-                    }
-                }
-            }
-
-        }
-
-        match self.timeout.poll() {
-                Err(e) => return Err((self.sink.take().unwrap(),
-                                      self.stream.take().unwrap(),
-                                      e)),
-                Ok(Async::NotReady) => (),
-                Ok(Async::Ready(_)) => {
-                    return Ok(Async::Ready((self.sink.take().unwrap(),
-                                            self.stream.take().unwrap(),
-                                            None)));
-                }
-        }
-
-        return Ok(Async::NotReady)
-    }
-}
-
-fn unreachable_to_udp_blocked<S,T>(tuple: (S, T, Error)) ->
-    Box<Future<Item=(S, T, Connectivity),
-               Error=(S, T, Error)>>
-    where S: Sink<SinkItem=(Vec<u8>, SocketAddr), SinkError=io::Error> + 'static,
-          T: Stream<Item=(Vec<u8>, SocketAddr), Error=io::Error> + 'static
-{
-    let (sink, stream, e) = tuple;
-
-    if e.raw_os_error() == Some(101) {
-        // Network unreachable
-        Box::new(ok((sink, stream, Connectivity::UdpBlocked)))
-    } else {
-        Box::new(err((sink, stream, e)))
     }
 }
 
@@ -199,7 +64,7 @@ impl UdpCodec for Codec {
     type In = (Vec<u8>, SocketAddr);
     type Out = (Vec<u8>, SocketAddr);
 
-    fn decode(&mut self, src: &SocketAddr, buf: &[u8]) -> Result<Self::In> {
+    fn decode(&mut self, src: &SocketAddr, buf: &[u8]) -> io::Result<Self::In> {
         Ok((buf.to_vec(), *src))
     }
 
@@ -209,39 +74,30 @@ impl UdpCodec for Codec {
     }
 }
 
-pub fn stun3489(bind_addr: SocketAddr,
+#[async]
+pub fn stun3489(bind_addr:   SocketAddr,
                 stun_server: SocketAddr,
-                handle: &Handle,
-                timeout: Duration)
-    -> Box<Future<Item=Connectivity, Error=io::Error>>
+                handle:      Handle,
+                timeout:     Duration)
+    -> io::Result<Connectivity>
 {
-    let sock = match UdpSocket::bind(&bind_addr.clone(), handle) {
-        Ok(s) => s,
-        Err(e) => return Box::new(err(e)),
-    };
-
-    let bind_addr = match sock.local_addr() {
-        Ok(a) => a,
-        Err(e) => return Box::new(err(e)),
-    };
-
+    let sock = UdpSocket::bind(&bind_addr.clone(), &handle)?;
+    let bind_addr = sock.local_addr()?;
     let (sink, stream) = sock.framed(Codec).split();
 
-    Box::new(stun3489_generic(Box::new(sink), stream.boxed(), bind_addr, stun_server, handle, timeout)
+    await!(stun3489_generic(Box::new(sink), stream.boxed(), bind_addr, stun_server, handle, timeout)
         .map(|(_,_,c)| c)
         .map_err(|(_,_,e)| e))
 }
 
-type ConnectivityFuture<S,T> = Box<Future<Item=(S, T, Connectivity),
-                                          Error=(S, T, io::Error)>>;
-
-pub fn stun3489_generic<S,T>(sink: S,
-                        stream: T,
-                        bind_addr: SocketAddr,
-                        stun_server: SocketAddr,
-                        handle: &Handle,
-                        timeout: Duration)
-    -> ConnectivityFuture<S,T>
+#[async]
+pub fn stun3489_generic<S,T>(sink:        S,
+                             stream:      T,
+                             bind_addr:   SocketAddr,
+                             stun_server: SocketAddr,
+                             handle:      Handle,
+                             timeout:     Duration)
+    -> Result<(S,T, Connectivity), (S,T,io::Error)>
     where S: Sink<SinkItem=(Vec<u8>, SocketAddr), SinkError=io::Error> + 'static,
           T: Stream<Item=(Vec<u8>, SocketAddr), Error=io::Error> + 'static
 {
@@ -250,68 +106,61 @@ pub fn stun3489_generic<S,T>(sink: S,
     let req_same_ip_diff_port = codec::Request::Bind(BindRequest { change_request: Some(ChangeRequest::Port), ..BindRequest::default() });
     let req_diff_ip_diff_port = codec::Request::Bind(BindRequest { change_request: Some(ChangeRequest::IpAndPort), ..BindRequest::default() });
 
-    let halt = Timeout::new(timeout, handle);
+    let halt = Timeout::new(timeout, &handle);
     let handle = handle.clone();
 
     let request1 = Request::new(sink, stream, stun_server.clone(), req, halt);
+    let (sink, stream, response) = await!(request1)?;
 
-    let result = request1.and_then(move |(sink, stream, response)| {
-        //println!("response1={:?}", response);
+    if let Some(Response::Bind(response)) = response {
+        let halt = Timeout::new(timeout, &handle);
+        let request2 = Request::new(sink, stream, stun_server.clone(), req_diff_ip_diff_port, halt);
+
+        let public_addr = response.mapped_address;
+        if bind_addr.ip() == public_addr.ip() {
+            // No NAT
+            let (sink, stream, response) = await!(request2)?;
+
+            if response.is_some() {
+                return Ok((sink, stream, Connectivity::OpenInternet(public_addr)));
+            } else {
+                return Ok((sink, stream, Connectivity::SymmetricFirewall(public_addr)));
+            }
+        }
+
+        // NAT detected
+        let (sink, stream, response) = await!(request2)?;
+        if response.is_some() {
+            return Ok((sink, stream, Connectivity::FullConeNat(public_addr)));
+        }
+
+        let halt = Timeout::new(timeout, &handle);
+        let request3 = Request::new(sink, stream, stun_server.clone(), req_same_ip_same_port, halt);
+        let (sink, stream, response) = await!(request3)?;
+
         if let Some(Response::Bind(response)) = response {
-            let halt = Timeout::new(timeout, &handle);
-            let request2 = Request::new(sink, stream, stun_server.clone(), req_diff_ip_diff_port, halt);
-
-            let public_addr = response.mapped_address;
-
-            if bind_addr.ip() == public_addr.ip() {
-                // No NAT
-                return Box::new(request2.and_then(move |(sink, stream, response)| {
-                    //println!("response2a={:?}", response);
-                    if response.is_some() {
-                        return Box::new(ok((sink, stream, Connectivity::OpenInternet(public_addr))));
-                    } else {
-                        return Box::new(ok((sink, stream, Connectivity::SymmetricFirewall(public_addr))));
-                    }
-                 })) as ConnectivityFuture<S,T>
+            if public_addr.ip() != response.mapped_address.ip() {
+                return Ok((sink, stream, Connectivity::SymmetricNat));
             }
 
-            // NAT detected
-            Box::new(request2.and_then(move |(sink, stream, response)| {
-                //println!("response2b={:?}", response);
-                if response.is_some() {
-                    return Box::new(ok((sink, stream, Connectivity::FullConeNat(public_addr)))) as ConnectivityFuture<S,T>;
-                }
+            let halt = Timeout::new(timeout, &handle);
+            let request4 = Request::new(sink, stream, stun_server.clone(), req_same_ip_diff_port, halt);
+            let (sink, stream, response) = await!(request4)?;
 
-                let halt = Timeout::new(timeout, &handle);
-                let request3 = Request::new(sink, stream, stun_server.clone(), req_same_ip_same_port, halt);
-                Box::new(request3.and_then(move |(sink, stream, response)| {
-                    //println!("response3={:?}", response);
-                    if let Some(Response::Bind(response)) = response {
-                        if public_addr.ip() != response.mapped_address.ip() {
-                            return Box::new(ok((sink, stream, Connectivity::SymmetricNat))) as ConnectivityFuture<S,T>;
-                        }
-
-                        let halt = Timeout::new(timeout, &handle);
-                        let request4 = Request::new(sink, stream, stun_server.clone(), req_same_ip_diff_port, halt);
-                        Box::new(request4.and_then(move |(sink, stream, response)| {
-                            if response.is_some() {
-                                Box::new(ok((sink, stream, Connectivity::RestrictedConeNat(public_addr))))
-                            } else {
-                                Box::new(ok((sink, stream, Connectivity::RestrictedPortNat(public_addr))))
-                            }
-                        }).or_else(unreachable_to_udp_blocked)) as ConnectivityFuture<S,T>
-                    } else {
-                         let msg = format!("Did not receive Some(BindResponse) but got {:?} instead!", response);
-                        Box::new(err((sink, stream, Error::new(ErrorKind::InvalidData, msg)))) as ConnectivityFuture<S,T>
-                    }
-                }).or_else(unreachable_to_udp_blocked)) as ConnectivityFuture<S,T>
-            }).or_else(unreachable_to_udp_blocked)) as ConnectivityFuture<S,T>
-         } else {
-             Box::new(ok((sink, stream, Connectivity::UdpBlocked))) as ConnectivityFuture<S,T>
-         }
-    }).or_else(unreachable_to_udp_blocked);
-
-    Box::new(result)
+            if response.is_some() {
+                Ok((sink, stream, Connectivity::RestrictedConeNat(public_addr)))
+            } else {
+                Ok((sink, stream, Connectivity::RestrictedPortNat(public_addr)))
+            }
+        } else {
+            let msg = format!("Did not receive Some(BindResponse) but got {:?} instead!", response);
+            let e = Error::new(ErrorKind::InvalidData, msg);
+            Err((sink, stream, e))
+        }
+    } else {
+        let e = Error::from_raw_os_error(NETWORK_UNREACHABLE);
+        Err((sink, stream, e))
+    }
 }
 
 
@@ -321,7 +170,6 @@ mod tests {
     use tokio_core::reactor::Core;
 
     use super::stun3489;
-    use super::Connectivity;
 
     #[test]
     fn it_works() {
@@ -332,10 +180,9 @@ mod tests {
         let mut core = Core::new().unwrap();
         let handle = core.handle();
 
-        let conn = stun3489(addr, server, &handle, timeout);
+        let conn = stun3489(addr, server, handle, timeout);
         let result = core.run(conn);
         assert!(result.is_ok());
-        assert_ne!(result.unwrap(), Connectivity::UdpBlocked);
     }
 
     #[test]
@@ -347,7 +194,7 @@ mod tests {
         let mut core = Core::new().unwrap();
         let handle = core.handle();
 
-        let conn = stun3489(addr, server, &handle, timeout);
+        let conn = stun3489(addr, server, handle, timeout);
         let result = core.run(conn);
         println!("{:?}", result);
         assert!(result.is_err());
@@ -362,11 +209,10 @@ mod tests {
         let mut core = Core::new().unwrap();
         let handle = core.handle();
 
-        let conn = stun3489(addr, server, &handle, timeout);
+        let conn = stun3489(addr, server, handle, timeout);
         let result = core.run(conn);
         println!("{:?}", result);
-        assert!(result.is_ok());
-        assert_eq!(result.unwrap(), Connectivity::UdpBlocked);
+        assert_eq!(result.err().unwrap().raw_os_error(), Some(::NETWORK_UNREACHABLE));
     }
 }
 
