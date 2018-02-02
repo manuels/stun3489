d736b56b02d52b2e94000c7a3741b99d955ff1a3
Manuel Schölling <manuel.schoelling@gmx.de>
2018-02-02 18:23:42 +0100

    Expect stream to be tokio_timer::TimoutStream to prevent 'stream already consumed' error

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,6 @@ pub mod codec;
 
 use std::io::Error;
 use std::io::ErrorKind;
-use std::time::Duration;
 use std::net::SocketAddr;
 use std::net::ToSocketAddrs;
 
@@ -21,7 +20,6 @@ use rand::Rng;
 
 use futures_await as futures;
 use futures::prelude::*;
-use tokio_timer::Timer;
 
 use codec::Request;
 use codec::Response;
@@ -71,13 +69,13 @@ pub fn codec<II,OO>(sink: OO, stream: II)
     (Box::new(sink), Box::new(stream))
 }
 
+/// `stream` should be a tokio_timer::TimeoutStream
 #[async]
 pub fn connectivity<B,S>(
         sink: O,
         stream: I,
         bind_addr: B,
-        stun_server: S,
-        timeout: Duration)
+        stun_server: S)
     -> Result<((O, I), Connectivity), ((O, I), std::io::Error)>
     where B: ToSocketAddrs + 'static,
           S: ToSocketAddrs + 'static,
@@ -86,14 +84,14 @@ pub fn connectivity<B,S>(
     let stun_server = stun_server.to_socket_addrs().unwrap().next().unwrap();
 
     let io = (sink, stream);
-    let (io, resp) = await!(change_request(io, stun_server, timeout, ChangeRequest::None))?;
+    let (io, resp) = await!(change_request(io, stun_server, ChangeRequest::None))?;
     if let Some(Response::Bind(resp)) = resp {
         let public_addr = resp.mapped_address;
         let public_ip = public_addr.ip();
 
         if bind_addr.ip() == public_ip {
             debug!("No NAT. Public IP ({}) == Bind IP ({})", bind_addr.ip(), public_ip);
-            let (io, resp) = await!(change_request(io, stun_server, timeout, ChangeRequest::IpAndPort))?;
+            let (io, resp) = await!(change_request(io, stun_server, ChangeRequest::IpAndPort))?;
             if resp.is_some() {
                 info!("OpenInternet: {}", public_addr);
                 return Ok((io, Connectivity::OpenInternet(public_addr)));
@@ -105,21 +103,21 @@ pub fn connectivity<B,S>(
         debug!("Public IP ({}) != Bind IP ({})", bind_addr.ip(), public_ip);
 
         // NAT detected
-        let (io, resp) = await!(change_request(io, stun_server, timeout, ChangeRequest::IpAndPort))?;
+        let (io, resp) = await!(change_request(io, stun_server, ChangeRequest::IpAndPort))?;
         if resp.is_some() {
             info!("FullConeNat: {}", public_addr);
             return Ok((io, Connectivity::FullConeNat(public_addr)));
         }
 
         debug!("No respone from different IP and Port");
-        let (io, resp) = await!(change_request(io, stun_server, timeout, ChangeRequest::Port))?;
+        let (io, resp) = await!(change_request(io, stun_server, ChangeRequest::Port))?;
         if let Some(Response::Bind(resp)) = resp {
             if resp.mapped_address.ip() != public_ip {
                 info!("SymmetricNat");
                 return Ok((io, Connectivity::SymmetricNat));
             }
 
-            let (io, resp) = await!(change_request(io, stun_server, timeout, ChangeRequest::Port))?;
+            let (io, resp) = await!(change_request(io, stun_server, ChangeRequest::Port))?;
             if resp.is_some() {
                 info!("RestrictedConeNat: {}", public_addr);
                 Ok((io, Connectivity::RestrictedConeNat(public_addr)))
@@ -137,7 +135,7 @@ pub fn connectivity<B,S>(
 }
 
 #[async]
-fn change_request(io: (O,I), stun_server: SocketAddr, timeout: Duration, req: ChangeRequest)
+fn change_request(io: (O, I), stun_server: SocketAddr, req: ChangeRequest)
     -> Result<((O, I), Option<Response>), ((O, I), std::io::Error)>
 {
     let req = codec::Request::Bind(BindRequest {
@@ -145,15 +143,14 @@ fn change_request(io: (O,I), stun_server: SocketAddr, timeout: Duration, req: Ch
         ..BindRequest::default()
     });
 
-    await!(send_request(io, stun_server, timeout, req))
+    await!(send_request(io, stun_server, req))
 }
 
 #[async]
-fn send_request(io: (O, I), stun_server: SocketAddr, timeout: Duration, req: Request)
+fn send_request(io: (O, I), stun_server: SocketAddr, req: Request)
     -> Result<((O, I), Option<Response>), ((O, I), std::io::Error)>
 {
     let (mut sink, mut stream) = io;
-    let timer = Timer::default();
 
     let mut rng = rand::thread_rng();
     let id: u64 = rng.gen();
@@ -165,17 +162,18 @@ fn send_request(io: (O, I), stun_server: SocketAddr, timeout: Duration, req: Req
 
     let stream = stream.skip_while(move |&(actual_id, ref _resp)| Ok(actual_id != id) );
 
-    match await!(timer.timeout_stream(stream, timeout).into_future()) {
+    match await!(stream.into_future()) {
         Ok((Some((_id, resp)), t)) => {
-            let stream = Box::new(t.into_inner());
+            let stream = Box::new(t);
             Ok(((sink, stream), Some(resp)))
         },
         Ok((None, t)) => {
-            let stream = Box::new(t.into_inner());
+            let stream = Box::new(t);
             Ok(((sink, stream), None))
         },
         Err((e, t)) => {
-            let stream = Box::new(t.into_inner());
+            debug!("Err = {:?}", e);
+            let stream = Box::new(t);
 
             if e.kind() == std::io::ErrorKind::TimedOut {
                 Ok(((sink, stream), None))
@@ -190,6 +188,8 @@ fn send_request(io: (O, I), stun_server: SocketAddr, timeout: Duration, req: Req
 fn test_connectivity() {
     use tokio_core::reactor::Core;
     use tokio_core::net::UdpSocket;
+    use std::time::Duration;
+    use tokio_timer::Timer;
 
     use codec::StunCodec;
 
@@ -202,8 +202,9 @@ fn test_connectivity() {
     let sock = UdpSocket::bind(&bind_addr, &handle).unwrap();
     let (sink, stream) = sock.framed(StunCodec).split();
 
+    let stream = Timer::default().timeout_stream(stream, Duration::from_secs(1));
     let job = connectivity(Box::new(sink), Box::new(stream), bind_addr,
-        server, Duration::from_secs(1));
+        server);
 
     match core.run(job) {
         Ok((_, _conn)) => (),
